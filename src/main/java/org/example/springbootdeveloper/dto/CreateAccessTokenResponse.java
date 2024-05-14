a386f54b6c283daf9cd905993bbc424196e454d8
NIsik419 <81894972+NIsik419@users.noreply.github.com>
2024-05-14 18:48:47 +0900

    Delete src directory

--- src/main/java/org/example/springbootdeveloper/dto/CreateAccessTokenResponse.java
+++
@@ -1,9 +0,0 @@
-package org.example.springbootdeveloper.dto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class CreateAccessTokenResponse{
-    private String accessToken;
-}
